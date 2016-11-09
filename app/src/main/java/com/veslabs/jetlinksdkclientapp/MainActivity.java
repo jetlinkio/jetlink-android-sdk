84df72538136541418e6b547d3f447049d04536c
Burhan ARAS <burhan.aras@veslabs.com>
2016-11-09 09:38:29 +0300

    small ui bug fixes

--- app/src/main/java/com/veslabs/jetlinksdkclientapp/MainActivity.java
+++ app/src/main/java/com/veslabs/jetlinksdkclientapp/MainActivity.java
@@ -11,6 +11,7 @@ import com.veslabs.jetlinklibrary.JetLinkApp;
 import com.veslabs.jetlinklibrary.config.JetLinkUIProperties;
 import com.veslabs.jetlinklibrary.config.JetLinkUser;
 import com.veslabs.jetlinklibrary.config.JetlinkConfig;
+import com.veslabs.jetlinklibrary.faq.FaqActivity;
 import com.veslabs.jetlinklibrary.messaging.JetLinkChatActivity;
 
 
@@ -25,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
         JetlinkConfig jetlinkConfig = new JetlinkConfig("android-developer-8fe2a96b-581c53a1", "c636d12159eb47e5aa6972cc3417249a");
         JetLinkUIProperties uiProperties = new JetLinkUIProperties();
-        uiProperties.setBackgroundColor(Color.MAGENTA);
         jetlinkConfig.setJetLinkUIProperties(uiProperties);
         JetLinkApp.getInstance(getApplicationContext()).init(jetlinkConfig);
 
@@ -45,5 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btnFaq= (Button) findViewById(R.id.btnFaq);
+        btnFaq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FaqActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
